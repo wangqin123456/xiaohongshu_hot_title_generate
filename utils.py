96d957771140d0c3a4e5e28581404e3d07483bdc
wangqin123456 <113087446+wangqin123456@users.noreply.github.com>
2024-06-19 13:48:18 +0800

    小红书爆款文案生成

---
+++ utils.py
@@ -0,0 +1,23 @@
+from prompt_template import system_template_text, user_template_text
+from langchain.prompts import ChatPromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain.output_parsers import PydanticOutputParser
+from xiaohongshu_model import Xiaohongshu
+
+def generate_xiaohongshu(theme,openai_api_key):
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_template_text),
+            ("user", user_template_text)
+        ]
+    )
+
+    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key,openai_api_base="https://api.aigc369.com/v1")
+    output_parser = PydanticOutputParser(pydantic_object=Xiaohongshu)
+    chain = prompt | model
+    result = chain.invoke(
+        {"parser_instructions": output_parser.get_format_instructions(),
+         "theme": theme}
+    )
+    return result
+
