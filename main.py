f8c77824a13bd4a2e2a9190a0062ebb937c63808
wangqin123456 <113087446+wangqin123456@users.noreply.github.com>
2024-06-19 14:00:15 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import streamlit as st
+
 from utils import generate_xiaohongshu
 
-st.title("爆款小红书AI写作助手 ✏️")
+
+st.header("爆款小红书AI写作助手 ✏️")
 with st.sidebar:
     openai_api_key = st.text_input("请输入OpenAI API密钥：", type="password")
     st.markdown("[获取OpenAI API密钥](https://platform.openai.com/account/api-keys)")
@@ -15,11 +17,9 @@ if submit and not openai_api_key:
 if submit and not theme:
     st.info("请输入生成内容的主题")
     st.stop()
-
 if submit:
     with st.spinner("AI正在努力创作中，请稍等..."):
         result = generate_xiaohongshu(theme, openai_api_key)
-
     st.divider()
     left_column, right_column = st.columns(2)
     with left_column:
@@ -36,3 +36,4 @@ if submit:
     with right_column:
         st.markdown("##### 小红书正文")
         st.write(result.content)
+
